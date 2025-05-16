6092e034cfeb953d378f8535971ce43e82298af0
EnzoMello <enzomelo333@gmail.com>
2025-05-15 21:26:42 -0300

    feat: adiciona atividade de heap binário.

--- atividades Sort´s/bubble_sort.c
+++ atividades Sort´s/bubble_sort.c
@@ -1,15 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void bubble_sort(int size, int *arr) {
-    int aux, i, j, troca = 0;
+    int aux, i, j, troca;
 
-    for (i = size-1; i >= 1; --i) {
+    for (i = size - 1; i >= 1; --i) {
+        troca = 0; // moveu para dentro do loop para reiniciar a cada passagem
         for (j = 0; j < i; j++) {
-            if (arr[j] > arr[j+1]) {
+            if (arr[j] > arr[j + 1]) {
                 aux = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = aux;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = aux;
                 troca = 1;
             }
         }
@@ -20,10 +22,21 @@ void bubble_sort(int size, int *arr) {
 }
 
 int main() {
-    int arr[] = {5, 1, 4, 9, 8, 100, 3};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    int size = 1000;
+    int arr[1000];
+
+    // Semente para números aleatórios diferentes a cada execução
+    srand(time(NULL));
+
+    // Preenche o vetor com valores aleatórios entre 0 e 9999
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 10000;
+    }
+
+    // Ordena o vetor
     bubble_sort(size, arr);
 
+    // Imprime os elementos ordenados
     for (int i = 0; i < size; i++) {
         printf("%d\n", arr[i]);
     }
